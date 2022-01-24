f0b7c3c91484a8bd67741a85a3877ec52aa0accb
aavnip <aavnip@chicagobooth.edu>
2022-01-24 04:21:46 +0000

    Loops Completed

--- loops_fizz_buzz.rb
+++ loops_fizz_buzz.rb
@@ -4,3 +4,20 @@
 # for multiples of three print "Fizz" instead of the number
 #   for multiples of five print "Buzz" instead of the number
 #   for numbers which are multiples of both three and five print "FizzBuzz"
+
+number = 0
+
+100.times do
+  number = number + 1
+  
+if number % 15 == 0
+  p "FizzBuzz"
+elsif number % 5 == 0 
+  p "Buzz" 
+elsif number % 3 == 0 
+  p "Fizz"
+else
+  p number
+end
+
+end
